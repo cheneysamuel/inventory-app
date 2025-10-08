ae70441dead0b141316599ac2f366ca18af002ae
Samuel Cheney <cheneysamuel@gmail.com>
2025-10-08 09:08:29 -0400

    fixing async issue

--- script.js
+++ script.js
@@ -1864,8 +1864,14 @@ async function runFullInitialization() {
     console.log('running full initialization...');
     try {
         await setCurrentUserFromSupabase();
-        await cacheLookupTables().then(() => {
-                                              
+        await cacheLookupTables().then((runAfterCachedInit());
+        } catch (error) {
+            console.error('Error during full initialization:', error);
+        }
+}
+
+async function runAfterCachedInit() {
+    
             displaySlocValue();
 
             populateManageOthersDropdown();
@@ -4235,6 +4241,7 @@ function setActiveSidebarButton(buttonId) {
     const activeBtn = document.getElementById(buttonId);
     if (activeBtn) activeBtn.classList.add('active');
 }
+
 
 
 
